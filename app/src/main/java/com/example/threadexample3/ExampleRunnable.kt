18de6afe6af0dabe223e5aa23179e5d423206935
widiasihrambee <widiasihrambe@gmail.com>
2022-05-08 16:51:19 +0700

    ThreadExample3

---
+++ app/src/main/java/com/example/threadexample3/ExampleRunnable.kt
@@ -0,0 +1,24 @@
+package com.example.threadexample3
+
+import android.content.ContentValues.TAG
+import android.util.Log
+
+class ExampleRunnable : Runnable {
+    var seconds = 0
+    var TAG = "MainActivity"
+
+    constructor(seconds: Int) {
+        this.seconds = seconds
+    }
+
+    override fun run() {
+        for (i in 1 until seconds) {
+            Log.d(TAG, "startThread: $i")
+            try {
+                Thread.sleep(1000)
+            } catch (ie: InterruptedException) {
+                ie.printStackTrace()
+            }
+        }
+    }
+}
